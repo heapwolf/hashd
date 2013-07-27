266e60ec90e6cd67946453e83de9e0a91f3649b2
hij1nx <paolo@async.ly>
2013-07-27 17:07:48 -0400

    first

---
+++ README.md
@@ -0,0 +1,18 @@
+# SYNOPSIS
+hash all the files in a specified path, appreciate .ignore files. uses `sha1` by default.
+
+# USAGE
+
+### Use as a lib
+```js
+var hashd = require('hashd')
+hashd('./path/to/files', { algorithm: 'md5', ignore: ['.gitignore', '.npmignore'] }, function(err, md5) {
+  console.log(md5)
+})
+```
+
+### Use from the cli
+```bash
+npm install hashd -g
+hashd ./path/to/files --algorithm sha1 --ignore '.gitignore .npmignore'
+```
---
+++ index.js
@@ -0,0 +1,29 @@
+var crypto = require('crypto')
+var fs = require('fs')
+var path = require('path')
+var ignore = require('fstream-ignore')
+
+function hashd(path, opts, callback) {
+
+  var shasum = crypto.createHash(opts.algorithm || 'sha1')
+
+  if (opts.ignores && !Array.isArray(opts.ignores)) {
+    opts.ignores = opts.ignores.split(' ')
+  }
+
+  ignore({ path: path, ignoreFiles: opts.ignores })
+    .on('child', function (child) {
+      if (child.type !== 'Directory') {
+        shasum.update(fs.readFileSync(child.path).toString())
+      }
+    })
+    .on('error', function(err) {
+      console.log(err)
+    })
+    .on('end', function() {
+      callback(null, shasum.digest('hex'))
+    })
+  ;
+}
+
+module.exports = hashd
